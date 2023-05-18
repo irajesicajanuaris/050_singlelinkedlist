f1fde29c897556207d8237f27b0b491c9f8b1449
irajesicajanuaris <jjiraaa1@gmail.com>
2023-05-18 19:23:49 +0700

    menghapus node

--- singlelinkedlist1/singlelinkedlist1.cpp
+++ singlelinkedlist1/singlelinkedlist1.cpp
@@ -160,6 +160,14 @@ int main() {
 					break;
 				}
 
+				int nim;
+				cout << "Masukkan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim: " << nim << " berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
 			}
 
 
